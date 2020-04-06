b96a7bcefd77be673dea21020aef63e1a91d56e7
Alexander Rau <alexanderrau86@yahoo.com>
2020-04-06 16:59:50 -0600

    First commit

---
+++ app/src/main/java/android/alexkotlinandroidprojects/expressyourself/MainActivity.kt
@@ -0,0 +1,21 @@
+package android.alexkotlinandroidprojects.expressyourself
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val name = "Nolan Bushnell"
+        val yearOfBirth = 1943
+        var currentYear = 2020
+        var age: Int
+
+        age = currentYear - yearOfBirth
+        Log.i("info", "$age")
+    }
+}
